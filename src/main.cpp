f70b5d84c37f65dc77d77da4f9b95a274144c3af
lekpar112-a11y <lekpar112@gmail.com>
2025-10-21 13:03:40 +0700

    Final reset of main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,11 @@
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println("Build successful!");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  delay(1000);
 }
--- src/usb_midi_if.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <Arduino.h>
-void usb_midi_init();
-void usb_midi_send_note_on(uint8_t ch,uint8_t note,uint8_t vel);
-void usb_midi_send_note_off(uint8_t ch,uint8_t note);
-void usb_midi_send_cc(uint8_t ch,uint8_t cc,uint8_t val);
-void usb_midi_send_pitchbend(uint8_t ch,int16_t val);
-void usb_midi_send_pc(uint8_t ch,uint8_t pc);
-void usb_midi_send_bank_select(uint8_t ch,uint16_t bank);
