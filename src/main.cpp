69afac75d5b51cc02f0d2c89190061374ee65946
lekpar112-a11y <lekpar112@gmail.com>
2025-10-27 22:48:58 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,28 @@
 #include <Arduino.h>
-#include <BLEMIDI_Transport.h>
+#include "Adafruit_BLE.h"
+#include "Adafruit_BluefruitLE_UART.h"
 
-// Definisikan port Serial
-#define HW_MIDI_SERIAL  Serial1
-#define BLE_MIDI_SERIAL Serial2
-#define USB_MIDI_SERIAL Serial
-
-// Konfigurasi Pin
-const uint8_t ROW_PINS[8] = {PA4, PA5, PA6, PA7, PB12, PB13, PB14, PB15};
-const uint8_t COL_PINS[8] = {PC13, PC14, PC15, PA8, PA11, PA12, PB3, PB4};
-const uint8_t PIN_PITCH_X     = PA0; 
-const uint8_t PIN_PITCH_Y     = PA1; 
-const uint8_t PIN_PITCH_WHEEL = PB0; 
-const uint8_t PIN_POT_BALANCE = PB1; 
-const uint8_t PIN_POT_MASTER  = PB10;
-const uint8_t PIN_EQ[3] = {PA5, PA6, PA7};
-const uint8_t PIN_ENCODER_A  = PB6;
-const uint8_t PIN_ENCODER_B  = PB7;
-const uint8_t PIN_ENCODER_SW = PB8;
-
-// Variabel
-const uint16_t MIDI_CHANNEL = 1; 
-const uint8_t BASE_NOTE = 48; 
-uint8_t padNoteMap[64];
-int currentOctaveOffset = 0;
-int transpose = 0;
-uint8_t matrixState[8][8] = {0};
-unsigned long lastDebounce[8][8] = {0};
-volatile bool encoderMoved = false;
-unsigned long lastScanTime = 0;
-
-// Fungsi Pengiriman MIDI
-// ... (semua fungsi midiSendNoteOn, dll.)
+Adafruit_BluefruitLE_UART ble(Serial2);
 
 void setup() {
-    // ...
+  Serial.begin(115200);
+  ble.begin(true);
+  ble.setMode(BLUEFRUIT_MODE_DATA);
+  ble.setDeviceName("STM32-BLE-MIDI");
 }
 
 void loop() {
-    // ...
+  // Kirim pesan Note On untuk C4
+  uint8_t noteOn[] = {0x90, 60, 127};
+  if (ble.isConnected()) {
+    ble.write(noteOn, sizeof(noteOn));
+  }
+  delay(1000);
+
+  // Kirim pesan Note Off untuk C4
+  uint8_t noteOff[] = {0x80, 60, 0};
+  if (ble.isConnected()) {
+    ble.write(noteOff, sizeof(noteOff));
+  }
+  delay(1000);
 }
