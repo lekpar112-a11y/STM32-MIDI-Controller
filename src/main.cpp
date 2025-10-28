96ba4fa3ff768288c434349f58f9702493d72540
lekpar112-a11y <lekpar112@gmail.com>
2025-10-28 17:32:52 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,100 @@
-#include <Arduino.h>
-#include "Adafruit_BLE.h"
-#include "Adafruit_BluefruitLE_UART.h"
+#include <USBComposite.h>
 
-Adafruit_BluefruitLE_UART ble(Serial2);
+// --- KONFIGURASI PIN ---
+// (Ini hanya contoh, sesuaikan dengan wiring Anda)
+
+// Matrix Scanning untuk Piano & Tombol
+const int ROW_PINS[] = {PA0, PA1, PA2, PA3, PA4, PA5, PA6, PA7}; // 8 Baris
+const int COL_PINS[] = {PB0, PB1, PB2, PB3, PB4, PB5, PB6, PB7}; // 8 Kolom
+
+// CD4051 untuk Potensiometer
+const int MUX_ANALOG_PIN_1 = PC0; // Pin analog untuk membaca MUX 1 (8 fader EQ)
+const int MUX_ANALOG_PIN_2 = PC1; // Pin analog untuk membaca MUX 2 (Vol, Bal)
+const int MUX_ADDR_PINS[] = {PB8, PB9, PB10}; // Pin alamat untuk kedua MUX
+
+// Rotary Encoder
+const int ENCODER_A = PB12;
+const int ENCODER_B = PB13;
+const int ENCODER_SW = PB14;
+
+// Tombol-tombol khusus
+const int CHORD_SCAN_SW = PC14;
+const int VELOCITY_SW = PC15;
+// ... tambahkan pin untuk tombol lainnya
+
+// --- INISIALISASI USB MIDI ---
+USBHID HID;
+USBMIDI MIDI;
+
+// --- VARIABEL GLOBAL ---
+byte noteStates[128] = {0}; // Menyimpan status setiap nada (ditekan/tidak)
+// ... tambahkan variabel lain yang dibutuhkan
 
 void setup() {
-  Serial.begin(115200);
-  ble.begin(true);
-  ble.setMode(BLUEFRUIT_MODE_DATA);
-  ble.setDeviceName("STM32-BLE-MIDI");
+  // Inisialisasi Serial1 untuk mengirim data ke ESP32
+  Serial1.begin(115200);
+
+  // Inisialisasi USB sebagai perangkat komposit (HID + MIDI)
+  USBComposite.begin();
+  
+  // Setup pin matrix
+  for (int i = 0; i < 8; i++) {
+    pinMode(ROW_PINS[i], OUTPUT);
+    pinMode(COL_PINS[i], INPUT_PULLUP);
+  }
+
+  // Setup pin MUX
+  pinMode(MUX_ANALOG_PIN_1, INPUT_ANALOG);
+  pinMode(MUX_ANALOG_PIN_2, INPUT_ANALOG);
+  for (int i = 0; i < 3; i++) {
+    pinMode(MUX_ADDR_PINS[i], OUTPUT);
+  }
+  
+  // ... inisialisasi pin lainnya
 }
 
 void loop() {
-  // Kirim pesan Note On untuk C4
-  uint8_t noteOn[] = {0x90, 60, 127};
-  if (ble.isConnected()) {
-    ble.write(noteOn, sizeof(noteOn));
-  }
-  delay(1000);
+  // Jalankan semua fungsi pemindaian secara berurutan
+  scanKeyboardAndMatrix();
+  readAnalogControls();
+  readEncoder();
+  readSpecialButtons();
+  
+  // Proses logika canggih (placeholder)
+  detectChord();
+}
 
-  // Kirim pesan Note Off untuk C4
-  uint8_t noteOff[] = {0x80, 60, 0};
-  if (ble.isConnected()) {
-    ble.write(noteOff, sizeof(noteOff));
-  }
-  delay(1000);
+// --- FUNGSI-FUNGSI PEMBANTU ---
+
+void scanKeyboardAndMatrix() {
+  // TODO: Implementasikan logika matrix scanning di sini.
+}
+
+void readAnalogControls() {
+  // TODO: Implementasikan pembacaan multiplexer CD4051.
+}
+
+void readEncoder() {
+  // TODO: Implementasikan logika pembacaan rotary encoder.
+}
+
+void readSpecialButtons() {
+  // TODO: Baca tombol-tombol khusus.
+}
+
+void detectChord() {
+  // TODO: Implementasikan algoritma deteksi akor.
+}
+
+// Fungsi terpusat untuk mengirim MIDI ke USB dan Serial (ESP32)
+void sendMIDI(byte status, byte data1, byte data2) {
+  // Kirim ke USB
+  midi_message_t msg = { (uint8_t)(status >> 4), status, data1, data2 };
+  MIDI.send(msg);
+  MIDI.flush();
+  
+  // Kirim juga ke ESP32
+  Serial1.write(status);
+  Serial1.write(data1);
+  Serial1.write(data2);
 }
